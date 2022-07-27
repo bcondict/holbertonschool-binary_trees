50f89a32d3465a1d50cd53ee1e51773b3ed3d485
Jesus <jesus.junc10@gmail.com>
2022-07-27 14:14:36 -0500

    16-binary_tree_is_perfect.c full correction :ambulance:

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -34,7 +34,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	if (left_height <= right_height)
+	if (left_height >= right_height)
 		return (left_height + 1);
 
 	return (right_height + 1);
@@ -52,10 +52,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t left_node = 0, right_node = 0;
-	binary_tree_t *right = tree->right, *left = tree->left;
+	binary_tree_t *right = NULL, *left = NULL;
 
 	if (!tree)
 		return (0);
+
+	right = tree->right;
+	left = tree->left;
 
 	if (binary_tree_is_leaf(tree))
 		return (1);
