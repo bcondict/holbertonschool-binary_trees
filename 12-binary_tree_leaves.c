c13db1dfa5b611e117b3c4ee104d31765a47deb4
Jesus <jesus.junc10@gmail.com>
2022-07-27 13:37:37 -0500

    12-binary_tree_leaves.c correction :hammer:

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -16,10 +16,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	binary_tree_leaves(tree->left);
 	binary_tree_leaves(tree->right);
-	if (tree->right && tree->left)
-		return (2);
-	if (tree->right || tree->left)
+
+	if (!tree->left && !tree->right)
 		return (1);
 
-	return (1);
+	return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
 }
